fd2ee623db328eb438ad411619eca1e5063fef33
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2021-12-04 10:54:29 +0700

    commit register

---
+++ admin/proses2.php
@@ -0,0 +1,30 @@
+
+<?php
+//Include file koneksi ke database
+include 'koneksi.php';
+
+//menerima nilai dari kiriman form pendaftaran
+$nama = $_POST['nama'];
+$email = $_POST['email']; //menampung data yang dikirim dari input username
+$password = $_POST['password']; //menampung data yang dikirim dari input password
+
+
+
+//Query input menginput data kedalam tabel anggota
+//   $sql="insert into admin (nama,email,password) values
+// 		('$nama','$email','$password')";
+        $sql = "INSERT INTO admin (nama,email,password)";
+        $sql .= "VALUES ('$nama', '$email', '$password')";
+//Mengeksekusi/menjalankan query diatas	
+  $hasil=mysqli_query($koneksi,$sql);
+
+//Kondisi apakah berhasil atau tidak
+  if ($hasil) {
+	header("location:sukses.php"); 
+  }
+else {
+echo"gagal";
+	exit;
+}  
+
+?>
