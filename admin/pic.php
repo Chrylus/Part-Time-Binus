364aebd7eae16f63beded4aca46676ccfb3d5c13
Chrylus <czar.soecahyo@binus.ac.id>
2021-12-09 10:14:36 +0700

    Update PIC

--- admin/pic.php
+++ admin/pic.php
@@ -3,13 +3,11 @@
   if(!isset($_SESSION["id"])){
     header("location:login.php");
   }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,7 +15,7 @@
     <meta name="author" content="">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>SB Admin 2 - Sort Table</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -27,6 +25,10 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Table sort -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 
 </head>
 
@@ -45,46 +47,43 @@
                 </div>
                 
                 <div class="sidebar-brand-text mx-3">
-                <?php 
-                    if (isset($_SESSION["id"])) {              
-                        echo($_SESSION['nama']);
-                    }
+                    <?php 
+                        if (isset($_SESSION["id"])) {              
+                            echo($_SESSION['nama']);
+                        }
 
-                    $connection = mysqli_connect('localhost', 'root', '', 'form_it');
-                    if(!$connection) {
-                        die("Database connection failed");
-                    }
+                        $connection = mysqli_connect('localhost', 'root', '', 'form_it');
+                        if(!$connection) {
+                            die("Database connection failed");
+                        }
 
-                    //Pengujian Apakah data akan diedit atau disimpan baru
-                    if(isset($_GET['hal']))
-	                {
-                        if($_GET['hal'] == "edit")
+                        //Pengujian Apakah data akan diedit atau disimpan baru
+                        if(isset($_GET['hal']))
                         {
-                            //Data akan di edit
-                            $edit = mysqli_query($connection,   "UPDATE admin 
-                                                                SET status = CASE WHEN status = 'Online' THEN 'Offline' ELSE 'Online' END
-                                                                WHERE id = '$_GET[id]'",
-                                                                );
-                            if($edit) //jika edit sukses
+                            if($_GET['hal'] == "edit")
                             {
-                                echo "<script>
-                                        alert('Edit data Sukses!');
-                                    </script>";
-                            }
-                            else
-                            {
-                                echo "<script>
-                                        alert('Edit data Gagal!!');
-                                    </script>";
+                                //Data akan di edit
+                                $edit = mysqli_query($connection,   "UPDATE admin 
+                                                                    SET status = CASE WHEN status = 'Online' THEN 'Offline' ELSE 'Online' END
+                                                                    WHERE id = '$_GET[id]'",
+                                                                    );
+                                if($edit) //jika edit sukses
+                                {
+                                    echo "<script>
+                                            alert('Edit data Sukses!');
+                                        </script>";
+                                }
+                                else
+                                {
+                                    echo "<script>
+                                            alert('Edit data Gagal!!');
+                                        </script>";
+                                }
                             }
                         }
-                    }
-                ?>    
-                
-                
+                    ?>                                
                 </div>
             </a>
-
             
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
@@ -98,11 +97,6 @@
 
             <!-- Divider -->
             <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <!-- <div class="sidebar-heading">
-                Interface
-            </div> -->
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
@@ -124,18 +118,16 @@
             </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="#">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Artikel</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="#">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Event</span></a>
             </li>
-
 
             <div class="sidebar-heading">
                 Administrator
@@ -147,23 +139,13 @@
                     <span>PIC</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="#">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Reminder</span></a>
             </li>
+            
             <!-- Divider -->
             <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            
-           
-                <!-- <a class="nav-link" href="tables.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a> -->
-
-
-                    
-            </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -172,9 +154,6 @@
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
-
-            
-
         </ul>
         <!-- End of Sidebar -->
 
@@ -192,51 +171,8 @@
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <!-- Topbar Search -->
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button" style="background-color:#0090D1">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-
-                        
-
-                        
-
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
@@ -244,50 +180,33 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                <?php 
-                            if (isset($_SESSION["id"])) {              
-                           echo($_SESSION['nama']);
-                            }
-                            
-                            ?>
+                                    <?php 
+                                    if (isset($_SESSION["id"])) {              
+                                        echo($_SESSION['nama']);
+                                    }
+                                    ?>
                                 </span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <!-- <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a> -->
-                                <!-- <div class="dropdown-divider"></div> -->
                                 <?php
                                 if (isset($_SESSION["id"])) {              
-                            echo '<a href="logout.php" class="dropdown-item"  data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Logout </a>';
-                            }
-                            else {
-                            echo '<a href="login.php" class="dropdown-item"  >
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Login </a>';
-                            }
-                            ?>
+                                    echo '<a href="logout.php" class="dropdown-item"  data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout </a>';
+                                }
+                                else {
+                                    echo '<a href="login.php" class="dropdown-item"  >
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Login </a>';
+                                }
+                                ?>
                             </div>
                         </li>
-
                     </ul>
-
                 </nav>
                 <!-- End of Topbar -->
 
@@ -296,7 +215,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Data Administrator</h1>
                     </div>
 
                     <!-- Content Row -->
@@ -313,37 +232,38 @@
                                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                             <thead>
                                                 <tr>
-                                                    <th>no</th>
+                                                    <th>No</th>
                                                     <th>Nama</th>
                                                     <th>Email</th>
                                                     <th>Status</th>
+                                                    <th>Command</th>
                                                 </tr>
                                             </thead>
-                                            <tfoot>
+                                            <!-- <tfoot>
                                                 <tr>
-                                                    <th>no</th>
+                                                    <th>No</th>
                                                     <th>Nama</th>
                                                     <th>Email</th>
                                                     <th>Status</th>
+                                                    <th>Command</th>
                                                 </tr>
-                                            </tfoot>
+                                            </tfoot> -->
                                             <tbody>
                                             <?php
-                            $no = 1;
-                            $tampil = mysqli_query($connection, "SELECT * from admin order by id desc");
-                            while($data = mysqli_fetch_array($tampil)) :
-
-                            ?>
-                            <tr>
-                            <td><?=$no++;?></td>
-                            <td><?=$data['nama']?></td>
-                            <td><?=$data['email']?></td>
-                            <td><?=$data['status']?></td>
-                            <td>
-                                <a href="pic.php?hal=edit&id=<?=$data['id']?>" class="btn btn-warning">Ubah Status</a>
-                            </td>
-                            </tr>
-                            <?php endwhile; //penutup perulangan while ?>           
+                                                $no = 1;
+                                                $tampil = mysqli_query($connection, "SELECT * from admin order by id desc");
+                                                while($data = mysqli_fetch_array($tampil)) :
+                                            ?>
+                                            <tr>
+                                                <td><?=$no++;?></td>
+                                                <td><?=$data['nama']?></td>
+                                                <td><?=$data['email']?></td>
+                                                <td><?=$data['status']?></td>
+                                                <td>
+                                                    <a href="pic.php?hal=edit&id=<?=$data['id']?>" class="btn btn-warning">Ubah Status</a>
+                                                </td>
+                                            </tr>
+                                            <?php endwhile; //penutup perulangan while ?>           
                                             </tbody>
                                         </table>
                                     </div>
