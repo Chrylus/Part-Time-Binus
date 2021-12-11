fa7ae1cf518838b5a4ae627a6bc13c584f92b3df
Chrylus <czar.soecahyo@binus.ac.id>
2021-12-11 13:34:34 +0700

    update On Progress Banner Card

--- admin/index.php
+++ admin/index.php
@@ -339,7 +339,7 @@
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> On Progress Ticket</div>
-                                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data5['overdue'];?></div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data2['On Progress'];?></div>
                                             </div>
                                             <div class="col-auto">
                                                 <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
