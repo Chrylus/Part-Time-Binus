d5b51517ae685eb80548c78d6335755b68ffe3fa
Chrylus <czar.soecahyo@binus.ac.id>
2021-12-13 11:45:35 +0700

    Update Index.php

--- admin/index.php
+++ admin/index.php
@@ -23,6 +23,14 @@
     $result3=mysqli_query($connection,$sql3);
     $data3=mysqli_fetch_assoc($result3);
 
+    $sql4="select count(status) as open_peminjaman from peminjaman WHERE (status='Open') AND MONTH (tanggal_start) = MONTH (CURDATE()) AND YEAR (tanggal_start) = YEAR (CURDATE())";
+    $result30=mysqli_query($connection,$sql4);
+    $data30=mysqli_fetch_assoc($result30);
+
+    $sql5="select count(status) as open_event from event WHERE (status='Open') AND MONTH (tanggal_start) = MONTH (CURDATE()) AND YEAR (tanggal_start) = YEAR (CURDATE())";
+    $result31=mysqli_query($connection,$sql5);
+    $data31=mysqli_fetch_assoc($result31);
+
     $completed="select count(status) as completed from complaint WHERE (status='Completed') AND MONTH (tanggal_start) = MONTH (CURDATE()) AND YEAR (tanggal_start) = YEAR (CURDATE())";
     $result4=mysqli_query($connection,$completed);
     $data4=mysqli_fetch_assoc($result4);
@@ -392,6 +400,44 @@
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"> Total Tiket</div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data1['ticket'];?></div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Pending Requests Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <a href = "tables_peminjaman.php">
+                                <div class="card border-left-dark shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"> Open Ticket Peminjaman</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data30['open_peminjaman'];?></div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Pending Requests Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <a href = "tables_event.php">
+                                <div class="card border-left-secondary shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"> Open Ticket Event</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data31['open_event'];?></div>
                                             </div>
                                             <div class="col-auto">
                                                 <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
